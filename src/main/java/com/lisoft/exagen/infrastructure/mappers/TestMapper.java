424cdc448593dff20c4357439993da3bf9d03b5a
Rubén Alvarado <ruben_aalvarado@outlook.com>
2025-11-07 21:53:30 -0600

    refactor: :recycle: Unused import deleted

--- src/main/java/com/lisoft/exagen/infrastructure/mappers/TestMapper.java
+++ src/main/java/com/lisoft/exagen/infrastructure/mappers/TestMapper.java
@@ -8,7 +8,6 @@ import com.lisoft.exagen.infrastructure.schemas.TestSchema;
 import com.lisoft.exagen.infrastructure.schemas.UserReference;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
