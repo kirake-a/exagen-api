dae4e2ef9f0510fa19054220ef7c6755a234ad71
Rubén Alvarado <ruben_aalvarado@outlook.com>
2025-11-07 21:49:36 -0600

    refactor: :recycle: Defining system's constants

--- src/main/java/com/lisoft/exagen/domain/utils/Constants.java
+++ src/main/java/com/lisoft/exagen/domain/utils/Constants.java
@@ -5,4 +5,8 @@ public class Constants {
 
     public static final String API_VERSION = "/api/v1";
     public static final String OPEN_API_VERSION = "v0.0.1";
+
+    public static final String UNEXPECTED_ERROR_MESSAGE = "An unexpected error occurred: ";
+    public static final String RESOURCE_NOT_FOUND_MESSAGE = "Resource not found: ";
+    public static final String INVALID_ARGUMENT_MESSAGE = "Invalid argument: ";
 }
