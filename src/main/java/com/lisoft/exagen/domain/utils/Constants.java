6358442c24f035cc47ce1fb91ca3d096b122acc9
Rubén Alvarado <ruben_aalvarado@outlook.com>
2025-11-26 00:29:39 -0600

    refactor: :recycle: New constants - refactor data validation

--- src/main/java/com/lisoft/exagen/domain/utils/Constants.java
+++ src/main/java/com/lisoft/exagen/domain/utils/Constants.java
@@ -7,9 +7,15 @@ public class Constants {
     public static final String OPEN_API_VERSION = "v0.0.1";
 
     public static final String UNEXPECTED_ERROR_MESSAGE = "An unexpected error occurred: ";
-    public static final String RESOURCE_NOT_FOUND_MESSAGE = "Resource not found: ";
     public static final String INVALID_ARGUMENT_MESSAGE = "Invalid argument: ";
 
     public static final String USER_ID_CANNOT_BE_NULL_MESSAGE = "User ID cannot be null";
     public static final String TEST_ID_CANNOT_BE_NULL_MESSAGE = "Test ID cannot be null";
+
+    public static final String QUESTION_NOT_FOUND_MESSAGE = "Question not found";
+    public static final String UNAUTHORIZED_ACCESS_2_QUESTION = "Maybe your not authorized to access this questions";
+    public static final String QUESTION_FOUND_SUCCESSFULLY = "Your question was found successfully";
+
+    public static final String CATEGORY_NOT_FOUND_MESSAGE = "Category not found";
+    public static final String UNAUTHORIZED_ACCESS_2_CATEGORY = "Maybe your not authorized to access this category";
 }
--- src/main/java/com/lisoft/exagen/domain/utils/DataValidator.java
+++ src/main/java/com/lisoft/exagen/domain/utils/DataValidator.java
@@ -1,5 +1,9 @@
 package com.lisoft.exagen.domain.utils;
 
+import com.lisoft.exagen.domain.exceptions.InvalidArgumentException;
+import com.lisoft.exagen.domain.exceptions.UnauthorizedAccessException;
+
+import java.util.List;
 import java.util.Objects;
 
 import static com.lisoft.exagen.domain.utils.Constants.USER_ID_CANNOT_BE_NULL_MESSAGE;
@@ -10,6 +14,24 @@ public class DataValidator {
     public static void validateUserId(String userId) {
         if (Objects.isNull(userId) || userId.isEmpty()) {
             throw new  IllegalArgumentException(USER_ID_CANNOT_BE_NULL_MESSAGE);
+        }
+    }
+
+    public static void validateQuestions2Add(List openQuestions, List closedQuestions) {
+        if (openQuestions.isEmpty() && closedQuestions.isEmpty()) {
+            throw new IllegalArgumentException("There are no questions to add");
+        }
+    }
+
+    public static void validateOwnership(String currentUserId, String resourceOwnerId) {
+        if (Objects.isNull(currentUserId) || currentUserId.isEmpty()) {
+            throw new InvalidArgumentException("Cannot validate owner with null or empty ownerId");
+        }
+        if (Objects.isNull(resourceOwnerId) || resourceOwnerId.isEmpty()) {
+            throw new InvalidArgumentException("Cannot validate owner with null or empty resourceId");
+        }
+        if (!currentUserId.equals(resourceOwnerId)) {
+            throw new UnauthorizedAccessException("You do not look like the resource owner");
         }
     }
 }
