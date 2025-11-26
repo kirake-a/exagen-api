698b0014b61906da80873dc82697f01e65d7fcb3
Rubén Alvarado <ruben_aalvarado@outlook.com>
2025-11-26 00:30:36 -0600

    refactor: :recycle: New method to questions mapper

--- src/main/java/com/lisoft/exagen/infrastructure/mappers/OpenQuestionMapper.java
+++ src/main/java/com/lisoft/exagen/infrastructure/mappers/OpenQuestionMapper.java
@@ -2,7 +2,9 @@ package com.lisoft.exagen.infrastructure.mappers;
 
 import com.lisoft.exagen.application.dtos.OpenQuestionDto;
 import com.lisoft.exagen.domain.models.OpenQuestion;
+import com.lisoft.exagen.infrastructure.schemas.CategorySchema;
 import com.lisoft.exagen.infrastructure.schemas.OpenQuestionSchema;
+import com.lisoft.exagen.infrastructure.schemas.UserReference;
 
 public class OpenQuestionMapper {
     private OpenQuestionMapper() {}
@@ -20,11 +22,42 @@ public class OpenQuestionMapper {
         );
     }
 
+    public static OpenQuestionSchema toSchema(OpenQuestion model) {
+        if (model == null) return null;
+
+        UserReference user = model.userId() != null ? new UserReference(model.userId()) : null;
+        CategorySchema category = null;
+
+        if (model.categoryId() != null) {
+            category = CategorySchema.builder()
+                    .id(model.categoryId())
+                    .build();
+        }
+
+        return OpenQuestionSchema.builder()
+                .id(model.id())
+                .statement(model.statement())
+                .response(model.response())
+                .user(user)
+                .category(category)
+                .build();
+    }
+
     public static OpenQuestionDto toOpenQuestionDto(OpenQuestion model) {
         return new OpenQuestionDto(
                 model.id(),
                 model.statement(),
                 model.response()
+        );
+    }
+
+    public static OpenQuestion openQuestionDtoToModel(OpenQuestionDto dto, String userId, Integer categoryId) {
+        return new OpenQuestion(
+                dto.id(),
+                dto.statement(),
+                dto.response(),
+                userId,
+                categoryId
         );
     }
 }
